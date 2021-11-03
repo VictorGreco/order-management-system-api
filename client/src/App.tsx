47967cf87749e4086efd5e4777a716df2d78bfb3
VictorGreco <victorgrecotech@gmail.com>
2021-11-03 02:44:37 +0100

    feature: added client scaffold

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.scss';
+
+const App: React.FC = () => (
+    <div className="wrapper">
+        <h1>Project scaffold 🚀</h1>
+    </div>
+);
+
+export default App;
